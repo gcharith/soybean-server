2b3c23e83632978aeb09102d0edfe14f5a07912b
Charith Grandhi <charithgrandhi@Chariths-MacBook-Air.local>
2025-12-01 18:54:54 -0500

    update pydantic schemas.py

--- soybean-backend/schemas.py
+++ soybean-backend/schemas.py
@@ -2,10 +2,20 @@ from pydantic import BaseModel, EmailStr
 from datetime import datetime
 from typing import Optional
 
-class UserCreate(BaseModel):
+##USER
+class UserBase(BaseModel):
     name: str
     email: EmailStr
-    password: str
+
+class UserCreate(UserBase):
+    password:str
+
+class UserOut(UserBase):
+    id: str
+    created_at: datetime
+
+    class Config:
+        orm_mode = True
 
 class UserOut(BaseModel):
     id: int
@@ -15,5 +25,44 @@ class UserOut(BaseModel):
 
     class Config:
         orm_mode = True
+
+
+
+##PREDICTION
+class PredictionBase(BaseModel):
+    image_url: str
+    predicted_label:str
+    confidence: Optional[float] = None
+    model_version: Optional[str] = None
+
+class PredictionCreate(PredictionBase):
+    pass 
+
+class PredictionOut(PredictionBase):
+    id: int
+    user_id: int
+    created_at: datetime
+
+    class Config:
+        orm_mode = True
+
+##FEEDBACK
+class FeedbackBase(BaseModel):
+    prediction_id: int
+    rating: Optional[int] = None
+    is_correct: Optional[bool] = None
+    comment: Optional[str] = None
+
+class FeedbackCreate(FeedbackBase):
+    pass 
+
+class FeedbackOut(FeedbackBase):
+    id: int
+    user_id: int
+    created_at: datetime
+
+    class Config:
+        orm_model = True
+
 
 
