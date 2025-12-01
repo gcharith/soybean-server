50a79e70d5f2cbabcf9b4b9db96e1c1e65c70234
Charith Grandhi <charithgrandhi@Chariths-MacBook-Air.local>
2025-12-01 18:11:06 -0500

    initial commit

---
+++ soybean-backend/schemas.py
@@ -0,0 +1,19 @@
+from pydantic import BaseModel, EmailStr
+from datetime import datetime
+from typing import Optional
+
+class UserCreate(BaseModel):
+    name: str
+    email: EmailStr
+    password: str
+
+class UserOut(BaseModel):
+    id: int
+    name: str
+    email: EmailStr
+    created_at: datetime
+
+    class Config:
+        orm_mode = True
+
+
